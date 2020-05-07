d6d98698cc56b83ea88aacd1b7a92a718179c19f
SumidhaNashier <sumidhanashier@gmail.com>
2020-05-07 12:19:12 +0530

    added test.txt

---
+++ AnotherClass.java
@@ -0,0 +1,12 @@
+package Sumidha_Training;
+
+public class AnotherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class_Sumidha obj= new obj();
+		
+
+	}
+
+}
---
+++ Class_Sumidha.java
@@ -0,0 +1,10 @@
+package Sumidha_Training;
+public class Class_Sumidha
+{
+	public String a= "sumidha";
+	public int b= 2000;
+	public char c= 'z';
+	public void msg() {
+		System.out.println("a " +b +"c");
+	}		
+}
